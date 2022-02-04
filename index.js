eaeb2ded9486aa0106f11d36db999eb8c9262dee
Lukelupus <62310522+Lukelupus@users.noreply.github.com>
2022-02-04 20:46:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImage1 = document.getElementById("playerOneDice").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+
+
+
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImage2 = document.getElementById("playerTwoDice").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+
+
+
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  }
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
